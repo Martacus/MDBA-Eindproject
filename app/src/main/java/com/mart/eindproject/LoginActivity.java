d8d2af43d923f40d03703abfa620a80f6290395c
Mart <mcpkmart@hotmail.nl>
2020-02-26 10:59:26 +0100

    First commit

---
+++ app/src/main/java/com/mart/eindproject/LoginActivity.java
@@ -0,0 +1,51 @@
+package com.mart.eindproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText Name;
+    private EditText Password;
+    private Button Login;
+
+    private Map<String, String> accounts = new HashMap<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        accounts.put("admin", "admintest");
+        accounts.put("user", "user");
+
+        Name = findViewById(R.id.username_text_field);
+        Password = findViewById(R.id.password_field);
+        Login = findViewById(R.id.login_button);
+
+        Login.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                validate(Name.getText().toString(), Password.getText().toString());
+            }
+        });
+    }
+
+    private void validate(String username, String password){
+        if(accounts.containsKey(username)){
+            if(password.contentEquals(password)){
+                Intent intent = new Intent(this, MainActivity.class);
+                startActivity(intent);
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/mart/eindproject/MainActivity.java
@@ -0,0 +1,22 @@
+package com.mart.eindproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        RecyclerView recyclerView = findViewById(R.id.screen_recycler_options);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+    }
+}
